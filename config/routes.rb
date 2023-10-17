7e3aa942448433565421d48ecddf6ff146877643
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2023-10-17 12:54:45 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  #get 'messages/index'?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
