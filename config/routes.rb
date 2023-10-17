c4e6cd8a950a4edb73de2710e58d9e1e0c950aca
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2023-10-17 14:24:02 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "messages#index"
+  #root to: "messages#index"?
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
